64f700580fa1719fcade58a1a9cdf22d7162111c
lucas <contato_lucassantos@outlook.com>
2021-03-12 13:31:26 -0300

    Redução do tempo de vida de um fumante
    Exercícios usando variáveis e entrada de dados

--- Variaveis-e-entrada-de-dados/ex3.15.py
+++ Variaveis-e-entrada-de-dados/ex3.15.py
@@ -4,6 +4,7 @@
 cigarros_por_dia = int(input('Quantos cigarros você fuma por dia? '))
 anos_fumando = int(input('Há quantos anos você fuma? '))
 reducao_vida_minutos = cigarros_por_dia * 10 * anos_fumando * 365
+
 # dia tem 24 horas e cada hora 60 minutos
 reducao_vida_dias = reducao_vida_minutos / (24 * 60)
-print('Você já perdeu {:.2f} dias de vida! '.format(reducao_vida_dias))
+print('Você já perdeu {:.2f} dias de vida pelo seu tempo fumando! '.format(reducao_vida_dias))
