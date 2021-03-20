b7589795c7365eda77aab507d5cb261cd099bd9a
lucas <contato_lucassantos@outlook.com>
2021-03-19 23:52:25 -0300

    Update ex4.3.py

--- Condicoes/ex4.3.py
+++ Condicoes/ex4.3.py
@@ -1,4 +1,4 @@
-#Programa 4.3 - Cálculo de Imposto de Renda
+# Programa 4.3 - Cálculo de Imposto de Renda
 """
     Exemplo importante:
     Salario 5.000
@@ -8,7 +8,7 @@ salario = float(input('Digite seu sálario: '))
 base = salario
 imposto = 0
 if base > 3000:
-    imposto = imposto + ((base - 3000 ) * 0.35)
+    imposto = imposto + ((base - 3000) * 0.35)
     base = 3000
 if base > 1000:
     imposto = imposto + ((base - 1000) * 0.20)
